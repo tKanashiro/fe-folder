3406b9e38961a6ccaf81998a7c5326b4c0305a22
Tammy Kanashiro <tammy.kanashiro@aligent.com.au>
2026-07-18 10:34:47 +0930

    Add obj mutable

---
+++ src/app/es17/components/temporal/ObjMutable.tsx
@@ -0,0 +1,57 @@
+import { Block } from "../Block";
+import { Code } from "../Code";
+import { Header } from "../Header";
+import { Result } from "../Result";
+import { Temporal } from "temporal-polyfill";
+
+
+export function ObjMutable() {
+    const startDate = new Date(2026, 1, 15);
+    const endDate = startDate;
+    endDate.setDate(endDate.getDate() + 7);
+
+    // Create a Date
+    const date1 = Temporal.PlainDate.from("2026-05-17");
+
+    // Add 7 days
+    const date2 = date1.add({ days: 7 });
+
+    return (
+        <>
+            <Header title="Mutable vs Immutable" description="Date objects are mutable. Temporal objects are immutable." />
+
+            <Block>
+                <Code code={`const startDate = new Date(2026, 1, 15);
+const endDate = startDate;
+
+endDate.setDate(endDate.getDate() + 7);`} />
+                <Result
+                    title="Start Date:"
+                    result={startDate.toString()}
+                />
+                <Result
+                    title="End Date:"
+                    result={endDate.toString()}
+                />
+            </Block>
+
+            <Block>
+                <Code code={`const startDate = Temporal.PlainDate.from("2026-05-17");
+const endDate = startDate.add({ days: 7 });`}
+                />
+                <Result
+                    title="Start Date:"
+                    result={date1.toString()}
+                    before={false}
+                />
+                <Result
+                    title="End Date:"
+                    result={date2.toString()}
+                    before={false}
+                    style="mt-5"
+                />
+            </Block>
+
+        </>
+    )
+}
--- src/app/es17/page.tsx
+++ src/app/es17/page.tsx
@@ -1,12 +1,14 @@
 
-import { TemporalBlock } from "./components/TemporalBlock";
+import { MonthBased } from "./components/temporal/MonthBased";
+import { ObjMutable } from "./components/temporal/ObjMutable";
 
 export default function Ecmascript() {
    
 
     return(
         <div className="max-w-[90%] m-auto">
-            <TemporalBlock />
+            <MonthBased />
+            <ObjMutable />
         </div>
     )
 }
